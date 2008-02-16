f740b5bb275a903cbdbd743eab2483e1049a6d05
Joshua Lande <joshualande@gmail.com>
2008-02-16 05:47:46 +0000

    I am updating the windows py2exe setup script so that it accounts for the fact that this program no longer uses Tix and that it dose not use Blt. I am also making it account for the stay .svn files all over the place.

--- build/setup_win_version1.py
+++ build/setup_win_version1.py
@@ -1,3 +1,4 @@
+# usage: python setup_win_version1.py py2exe
 import sys
 import os.path
 import shutil
@@ -10,27 +11,18 @@ import py2exe
 def listdirFull(thedir):
     """ This is just os.listdir() but it returns the full path names. """
     files = os.listdir(thedir)
-    for loop in range(len(files)):
-        files[loop] = thedir+os.sep+files[loop]
-    return files
+    list = []
+    for file in files:
+        if file != '.svn':
+            list.append(thedir+os.sep+file)
+    return list
 
 allXBMS = listdirFull(r"..\source\version1\xbms")
 allStandardQ = listdirFull(r"..\source\version1\StandardQ")
 
-setup(name='',
-      description="",
-      version='1',
-      scripts=[r'..\source\version1\AreaDiffractionMachine.py'],
+setup(console=[r'..\source\version1\AreaDiffractionMachine.py'],
       data_files = [('.',[r"..\source\version1\colormaps.txt",
-                         r"..\source\version1\BLT24.dll",
-                         r"..\source\version1\tix8183.dll",
                          r"..\source\version1\tips_and_tricks.html"]),
                     ('xbms',allXBMS),
                     ('StandardQ',allStandardQ)])
-
-if not os.path.exists(r"dist\AreaDiffractionMachine\tcl\tix8.1"):
-    # if you are building the program somewhere else,
-    # you might need to copy tix8.1 from somewhere else.
-    shutil.copytree(r"C:\Program Files\Python21\tcl\tix8.1",
-    r"dist\AreaDiffractionMachine\tcl\tix8.1")
 
