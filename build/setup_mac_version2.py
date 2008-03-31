87dce24cb10f2b92715f83bf1041772c234be77f
Joshua Lande <joshualande@gmail.com>
2008-03-31 01:47:49 +0000

    Moving all of the tentative progress with the program to a new folder called version2. I will next copy all of the work as of version 1.0.3 back into the current subversion into the version1 folder. This will allow me to have 2 concurrent versions of the program.

---
+++ build/setup_mac_version2.py
@@ -0,0 +1,48 @@
+import sys
+import os.path
+import shutil
+
+usage = """Usage: sudo python setup_mac_version2.py py2app --iconfile=landeBMWIcon.icns --version=X.X.X"""
+
+# figure out the version of the program
+versionstring = sys.argv.pop()
+
+if versionstring[0:10] != "--version=":
+    print usage
+    sys.exit(2)
+
+version = versionstring[10:]
+
+
+def listdirFull(thedir):
+    """ This is just os.listdir() but it returns the full path names. """
+    files = os.listdir(thedir)
+    for loop in range(len(files)):
+        files[loop] = thedir+os.sep+files[loop]
+    return files
+
+allXBMS = listdirFull(r"../source/version2/xbms")
+allStandardQ = listdirFull(r"../source/version2/StandardQ")
+
+from setuptools import setup,Extension
+
+setup(
+    name = "Area Diffraction Machine",
+    version = version,
+    data_files = [('.',['../source/version2/colormaps.txt']),
+                  ('.',['../source/version2/tips_and_tricks.html']),
+                  ('.',['../source/version2/ChangeLog.txt']),
+                  ('xbms',allXBMS),
+                  ('StandardQ',allStandardQ)
+                 ],
+    app=["../source/version2/AreaDiffractionMachine.py"],
+    setup_requires=['py2app'],
+)
+
+os.popen('sudo rm -rf *.dmg').close()
+print "Creating the disk image"
+os.popen('sudo mv dist "Area Diffraction Machine v%s"' % version).close()
+os.popen('sudo mv "Area Diffraction Machine v%s/Area Diffraction Machine.app" "Area Diffraction Machine v%s/Area Diffraction Machine v%s.app"' % (version,version,version)).close()
+os.popen('sudo hdiutil create -srcfolder "Area Diffraction Machine v%s" "Area Diffraction Machine v%s.dmg"' % (version,version) ).close()
+os.popen('sudo rm -rf "Area Diffraction Machine v%s"' % version).close()
+os.popen('sudo rm -rf "build"').close()
---
+++ build/setup_win_version2.py
@@ -0,0 +1,53 @@
+import sys
+import os
+import shutil
+
+
+usage = """Usage: python setup_win_version2.py py2exe --version=X.X.X"""
+
+# figure out the version of the program
+versionstring = sys.argv.pop()
+
+if versionstring[0:10] != "--version=":
+    print usage
+    sys.exit(2)
+version = versionstring[10:]
+
+
+sys.path.append(r"..\source\version2")
+
+from distutils.core import setup
+import py2exe
+
+def listdirFull(thedir):
+    """ This is just os.listdir() but it returns the full path names. """
+    files = os.listdir(thedir)
+    list = []
+    for file in files:
+        if file != '.svn':
+            list.append(thedir+os.sep+file)
+    return list
+
+allXBMS = listdirFull(r"..\source\version2\xbms")
+allStandardQ = listdirFull(r"..\source\version2\StandardQ")
+
+setup(name='Area Diffraction Machine',
+      version=version,
+      description='Analyze x-ray diffraction data.',
+      console=[{"script":r"..\source\version2\AreaDiffractionMachine.py",
+      "icon_resources":[(1,"LandeBMWIcon.ico")]}],
+      data_files = [('.',[r"..\source\version2\colormaps.txt",
+                          r"..\source\version2\ChangeLog.txt",
+                         r"..\source\version2\tips_and_tricks.html"]),
+                    ('xbms',allXBMS),
+                    ('StandardQ',allStandardQ)])
+
+
+print 'clean up directory'
+os.popen('RMDIR /s /q build')
+os.popen('del /s "Area Diffraction Machine v*"')
+os.popen('move dist "Area Diffraction Machine v%s"' % version)
+os.popen('move "Area Diffraction Machine v%s\AreaDiffractionMachine.exe" "Area Diffraction Machine v%s\Area Diffraction Machine v%s.exe"' % (version,version,version))
+print 'zipping up the folder'
+os.popen('zip -r "Area Diffraction Machine v%s.zip" "Area Diffraction Machine v%s"' % (version,version))
+os.popen('RMDIR /s /q "Area Diffraction Machine v%s"' % (version))
